4543b2a86b6ff737e8f17e4a84b2cb04813da3c2
Thomas Rose <thomas.rose@ionos.com>
2024-06-30 17:55:20 +0200

    add example page

---
+++ html/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Form submission handler
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const data = {};
+        formData.forEach((value, key) => data[key] = value);
+
+        alert("Vielen Dank! Ihre Nachricht wurde erfolgreich versendet. Wir werden uns in Kürze bei Ihnen melden.")
+
+    });
+});
