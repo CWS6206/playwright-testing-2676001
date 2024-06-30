c933c357ebfefb0893d9d8295b6942f8ebb0b9ea
Thomas Rose <thomas.rose@ionos.com>
2024-06-30 18:20:12 +0200

    add a11y tests

---
+++ tests/smoke-tests.spec.js
@@ -0,0 +1,27 @@
+const { test, expect } = require('@playwright/test')
+
+test('name field is required', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/html/index.html')
+
+  await page.click('button[type="submit"]')
+
+  const isRequired = await page.getAttribute('#name', 'required')
+  expect(isRequired).not.toBeNull()
+})
+
+test('link named "kontakt" leads to contact.html', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/html/index.html')
+
+  await page.click('text=Kontakt')
+
+  expect(page.url()).toContain('contact.html')
+})
+
+test('footer contains "Beispielunternehmen"', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/html/index.html')
+
+  const footer = await page.$('footer')
+  const footerText = await footer.textContent()
+
+  expect(footerText).toContain('Beispielunternehmen')
+})
